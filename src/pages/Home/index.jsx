afa18f76fc33c6caa5b4f5810326bc243a635ee3
Igor <igorjbahia@gmai.com>
2023-05-27 08:23:02 -0300

    :tada: first commit.

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './style.css'
+
+function Home() {
+
+  return (
+    <div className='container'>
+      <h1>Hello World</h1>
+
+    </div>
+
+  )
+}
+
+export default Home
